c6ec050fed65439c665a67f7f3b1b89492b98759
Yash Verma <yashv@lambdatest.com>
2025-05-24 19:35:43 +0530

    configure useformStatus hook

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import { UseExample3 } from './components/useExample3/Message';
 import { UseExampleContext } from './components/useExampleContext/Theme';
 import { ActionExample1 } from './components/actionExample1/Posts';
 import { ActionExample2 } from './components/actionExample2/ShoppingCart';
+import { UseFormStatusExample } from './components/useFormStatus/Posts';
+import AddToCartForm from './components/useFormStateExample/AddToCartForm';
 import './App.css'
 function App() {
  
@@ -23,9 +25,26 @@ function App() {
         <Route path='use-example-3' element={<UseExample3 />} />
         <Route path='use-example-context' element={<UseExampleContext />} />
         <Route path='action-example-1' element={<ActionExample1 />} />
-        {/*
-        <Route path='use-example-2' element={<UseExample2 />} />
-        <Route path='use-example-3' element={<UseExample3 />} /> */}
+        <Route path='action-example-2' element={<ActionExample2 />} />
+        <Route
+          path='useformstatus-example'
+          element={<UseFormStatusExample />}
+        />
+         <Route
+          path='useformstate-example'
+          element={
+            <>
+              <AddToCartForm
+                itemID='1'
+                itemTitle='JavaScript: The Definitive Guide'
+              />
+              <AddToCartForm
+                itemID='2'
+                itemTitle='JavaScript: The Good Parts'
+              />
+            </>
+          }
+        />
         </Route>
       
         
